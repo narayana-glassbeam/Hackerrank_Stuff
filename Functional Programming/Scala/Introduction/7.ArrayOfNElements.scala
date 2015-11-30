2d997ad4616371c8852c90ea29adc5fff985bd1c
leviathan88 <levigithuber@gmail.com>
2015-11-30 19:33:17 +0100

    scala challange

---
+++ Functional Programming/Scala/Introduction/7.ArrayOfNElements.scala
@@ -0,0 +1,21 @@
+ def f(num: Int) : List[Int] = {
+     val list = List.range(0, num)
+     list
+ }
+
+/*Problem Statement
+
+Create an array of N integers, where N is specified as input. This can be any array with N integers. For example, for N=4 you could return [1,1,1,1] or [1,2,3,4]
+
+Sample Input
+
+3
+Sample of the Return Value
+
+[1, 2, 3]
+Output Format
+The output only indicates the number of elements contained in the list, array or vector you created.
+
+Constraints
+N <= 100
+The members of the List/Vector/Array returned should be integers.*/
