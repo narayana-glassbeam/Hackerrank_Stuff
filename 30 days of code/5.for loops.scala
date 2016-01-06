dc781b8ecadc5432cd1b8ee3743937eb239ef3d9
leviathan88 <levigithuber@gmail.com>
2016-01-06 07:58:11 +0100

    adding loops for 30 days of code

---
+++ 30 days of code/5.for loops.scala
@@ -0,0 +1,36 @@
+object Solution {
+    def main(args: Array[String]) {
+        for (_ <- 0 until readInt)
+        {
+            val arr = readLine.split(' ').toList.map(_.toInt)
+            for (i <- 0 until arr(2)) {
+                var num = arr(0)
+                for (j <- 0 to i) {
+                    num += ((Math.pow(2,j)).toInt*arr(1))
+                    }
+                print(num+" ")
+                if(arr(2)-i==1)println
+            }
+        }
+    }
+}
+
+/*Problem Statement
+
+Welcome to Day 5! Check out the video review of loops here, or just jump right into the problem.
+
+In this problem you will test your knowledge of loops. Given three integers a, b, and N, output the following series:
+
+a+20b,a+20b+21b,......,a+20b+21b+...+2N−1b
+Input Format
+
+The first line will contain the number of testcases T. Each of the next T lines will have three integers, a, b, and N.
+
+Constraints
+
+0≤T≤500
+0≤a,b≤50
+1≤N≤15
+Output Format
+
+Print the answer to each test case in a separate line.*/
