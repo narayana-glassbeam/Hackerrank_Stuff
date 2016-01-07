568abf8abda1840f72862c1a2f2c8f6c47b6c46b
leviathan88 <levigithuber@gmail.com>
2016-01-07 13:56:55 +0100

    pangram solution for scala

---
+++ Algorithms/Scala solutions/strings/pangram.scala
@@ -0,0 +1,42 @@
+/*Problem Statement
+
+Roy wanted to increase his typing speed for programming contests. So, his friend advised him to type the sentence "The quick brown fox jumps over the lazy dog" repeatedly, because it is a pangram. (Pangrams are sentences constructed by using every letter of the alphabet at least once.)
+
+After typing the sentence several times, Roy became bored with it. So he started to look for other pangrams.
+
+Given a sentence s, tell Roy if it is a pangram or not.
+
+Input Format
+
+Input consists of a string s.
+
+Constraints
+Length of s can be at most 103 (1≤|s|≤103) and it may contain spaces, lower case and upper case letters. Lower-case and upper-case instances of a letter are considered the same.
+
+Output Format
+
+Output a line containing pangram if s is a pangram, otherwise output not pangram.*/
+
+object Solution {
+  def main(args: Array[String]) {
+    println(if(readLine.replaceAll(" ","").toLowerCase.distinct.size == 26) "pangram" else "not pangram")
+  }
+}
+
+//similar solution
+object Solution {
+  def main(args: Array[String]) {
+    println(if(readLine.replaceAll(" ","").toLowerCase.toSet.size == 26) "pangram" else "not pangram")
+  }
+}
+
+//my first solution, though lame, it's actually not that slower
+object Solution {
+    def main(args: Array[String]) {
+        val sentence = readLine.toLowerCase
+        val letters = ('a' to 'z' by 1).toList
+        val s = letters.map(sentence.contains(_))
+        val isPangram = !s.contains(false)
+        println(if(isPangram) "pangram" else "not pangram")
+    }
+}
