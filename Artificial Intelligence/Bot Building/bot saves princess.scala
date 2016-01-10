7ce1f70b6bf06014ca6abeb3b8d6b86e6ea1fccc
leviathan88 <levigithuber@gmail.com>
2016-01-10 13:43:49 +0100

    adding AI domain and first challenge bot saves princes for scala

---
+++ Artificial Intelligence/Bot Building/bot saves princess.scala
@@ -0,0 +1,20 @@
+object Solution {
+    def main(args: Array[String]) = {
+        val N = readInt
+        var arr = new Array[String](N)
+        for (i <- 0 until N) {
+            arr(i) = readLine
+        }
+        val mPos = arr.mkString.indexOf('m')
+        val pPos = arr.mkString.indexOf('p')
+
+        var vertical = mPos/N - pPos/N
+        var horizontal = mPos%N - pPos%N
+
+        if(vertical < 0) for(_ <- vertical until 0) println("DOWN")
+        else for(_ <- 0 until vertical) println("UP")
+        if(horizontal < 0) for(_ <- horizontal until 0) println("RIGHT")
+        else for(_ <- 0 until horizontal) println("LEFT")
+            }
+
+}
