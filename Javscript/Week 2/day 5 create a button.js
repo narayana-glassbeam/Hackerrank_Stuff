0c90c6f341c90399f236250e65e9714e5223bd08
leviathan88 <levigithuber@gmail.com>
2016-03-05 09:57:34 +0100

    day 5 create a button

---
+++ Javscript/Week 2/day 5 create a button.js
@@ -0,0 +1,7 @@
+var buttocks = document.createElement("Button");
+buttocks.id = "btn";
+buttocks.innerHTML = 0;
+document.body.appendChild(buttocks);
+buttocks.addEventListener("click", function() {
+                buttocks.innerHTML ++;
+            });
