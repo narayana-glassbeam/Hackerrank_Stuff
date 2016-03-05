c5c1eddd59870138fe9fd2bb142dca076d68807c
leviathan88 <levigithuber@gmail.com>
2016-03-05 09:35:59 +0100

    day 4 IIFE JS

---
+++ Javscript/Week 2/day 4 basics of FP (IIFE).js
@@ -0,0 +1,13 @@
+var captcha_check = (function(){
+    var  actual_captcha = Math.floor(Math.random(100)*Math.pow(10, 5));
+
+    return {
+        check: function(guess){
+        if(guess === this.actual_captcha){
+            console.log("You have written it incorrectly.");
+        } else {
+            console.log("You have written it correctly.");
+        }
+    }
+    }
+})();
