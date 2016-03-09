7367984f1d31a2841dd5d88818d632c4a50d73b9
leviathan88 <levigithuber@gmail.com>
2016-03-09 11:17:54 +0100

    adding first java solutions

---
+++ Java/Introduction/stdin stdout 1.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int a=sc.nextInt();
+      int b=sc.nextInt();
+      int c=sc.nextInt();
+
+      System.out.println(a);
+      System.out.println(b);
+      System.out.println(c);
+    }
+}
+
+/*In most of the HackerRank challenges, you need to read input from stdin (standard input) and write your output in stdout (standard output).
+
+One way to take input from stdin is to use the Scanner class and read in from System.in.
+
+You can write your output to stdout by simply using the System.out.println(String) function.
+
+In this problem, you need to read 3 integers from stdin and print them in stdout.*/
---
+++ Java/Introduction/welcome to java.java
@@ -0,0 +1,11 @@
+public class Solution {
+   public static void main(String []argv)
+   {
+      System.out.println("Hello World.");
+      System.out.println("Hello Java.");
+   }
+}
+
+/*Welcome to the world of Java! Just print "Hello World." and "Hello Java." in two separate lines to complete this challenge.
+
+The code stub in the editor already creates the main function and solution class. */
