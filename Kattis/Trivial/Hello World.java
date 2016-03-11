c4201f86d7c891a6e8846c6581bde38e92b4e49a
leviathan88 <levigithuber@gmail.com>
2016-03-11 18:54:38 +0100

    adding first Kattis chll for java (trivial)

---
+++ Kattis/Trivial/Hello World.java
@@ -0,0 +1,15 @@
+/**
+ * Created by umard on 11.03.2016..
+ */
+public class Mainz {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+
+/*Input
+There is no input for this problem.
+
+Output
+Output should contain one line, containing the string “Hello World!”.*/
