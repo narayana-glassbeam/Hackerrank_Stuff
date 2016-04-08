c6360fda910bd6e2868e8cadc863b1c8afa877bb
leviathan88 <levigithuber@gmail.com>
2016-04-08 09:35:33 +0200

    CW JS chl remove first and last char

---
+++ CodeWars/Javascript/8kyu/Remove first and last char.js
@@ -0,0 +1,14 @@
+removeChar=str=>{
+  return str.slice(1).slice(0,str.length-2)
+}
+
+//even better
+removeChar=str=>{
+  return str.slice(1,-1)}
+
+
+/*
+
+It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter.
+
+*/
