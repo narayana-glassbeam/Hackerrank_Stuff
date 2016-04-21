05d642009307c031731949f8bcadc87e53943423
leviathan88 <levigithuber@gmail.com>
2016-04-21 17:59:47 +0200

    CW JS 5Kyu Two Joggers

---
+++ CodeWars/Javascript/5kyu/TwoJoggers.js
@@ -0,0 +1,26 @@
+function nbrOfLaps(x, y) {
+  let gcd=(x,y)=>((y==0) ? x : gcd(y,x%y))
+  let num = x*y / gcd(x,y)
+  return [num/x, num/y];
+}
+
+/*Description:
+
+Two Joggers
+
+Description
+
+Bob and Charles are meeting for their weekly jogging tour. They both start at the same spot called "Start" and they each run a different lap, which may (or may not) vary in length. Since they know each other for a long time already, they both run at the exact same speed.
+
+Task
+
+Your job is to complete the function nbrOfLaps(x, y) that, given the length of the laps for Bob and Charles, finds the number of laps that each jogger has to complete before they meet each other again, at the same time, at the start.
+
+The function takes two arguments:
+
+The length of Bob's lap (larger than 0)
+The length of Charles' lap (larger than 0)
+The function should return an array containing exactly two numbers:
+
+The first number is the number of laps that Bob has to run
+The second number is the number of laps that Charles has to run*/
