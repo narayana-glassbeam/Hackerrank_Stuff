f00895828063f440d35997dbf96c813cb42845a4
leviathan88 <levigithuber@gmail.com>
2016-06-07 12:46:20 +0200

    CW JS 8Kyu Fix hello world message object

---
+++ CodeWars/Javascript/8kyu/Fix Hello World message object.js
@@ -0,0 +1,13 @@
+function run() {
+    "use strict";
+
+    return {
+       helloWorld: function () {
+       let str = { message: "Hello World"};
+       return str;
+    }};
+}
+/*
+Description:
+
+Fix the below javascript code without removing any lines of code. The intended output is Hello Worl*/d.
