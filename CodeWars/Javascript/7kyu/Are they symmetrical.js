ca9255312941c296dce988dd8b77915137840e05
leviathan88 <levigithuber@gmail.com>
2016-07-10 19:38:16 +0200

    CW JS 7 Kyu are they symmetrical

---
+++ CodeWars/Javascript/7kyu/Are they symmetrical.js
@@ -0,0 +1,9 @@
+sc=o=>(o.toString()=== (o instanceof Array ? o.reverse().toString() : o.toString().split("").reverse().join("")))
+
+/*Coding 3min: Are they symmetrical?
+
+This is the simple version of Shortest Code series. If you need some challenges, please try the challenge version
+
+Task:
+
+Give you an obj, it can be 3 types: string, number and number array, Check that they are symmetrical or not, return a boolean value*/
