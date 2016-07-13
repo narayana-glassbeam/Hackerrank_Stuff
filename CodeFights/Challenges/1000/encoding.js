ccb74f17ffe023305875385d0a0c4dc98cb6f763
leviathan88 <levigithuber@gmail.com>
2016-07-13 16:09:22 +0200

    CF 1000 encoding

---
+++ CodeFights/Challenges/1000/encoding.js
@@ -0,0 +1,15 @@
+encoding=n=>[...n+''].map(x=>" ihgfedcba"[x]).join("")
+
+/*Yet another reverse challenge. Have fun!
+
+[time limit] 4000ms (js)
+[input] integer number
+
+A number with no zeroes in it.
+
+Constraints:
+1 ≤ number < 109.
+
+[output] string
+
+A string of characters in range ['a' - 'i'].*/
