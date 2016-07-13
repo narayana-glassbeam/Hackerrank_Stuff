784b6988da2ddef8bf8c651a9ed41054d4dc3597
leviathan88 <levigithuber@gmail.com>
2016-07-13 12:35:41 +0200

    CF 1000 productTcudorp

---
+++ CodeFights/Challenges/1000/productTcudorp.js
@@ -0,0 +1,11 @@
+productTcudorp=n=>n*[...n+''].reverse().join('')
+
+T/*his is a reverse challenge. Have fun!
+
+[time limit] 4000ms (js)
+[input] integer n
+
+Constraints:
+0 ≤ n ≤ 1000.
+
+[output] integer*/
