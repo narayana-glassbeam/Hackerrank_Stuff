7ab30654a8f75215775d42aea2c665612d5fe7c4
leviathan88 <levigithuber@gmail.com>
2016-07-13 11:37:09 +0200

    CW 8kyu Find max and min

---
+++ CodeWars/Javascript/8kyu/Find Maximum and Minimum Values of a List.js
@@ -0,0 +1,7 @@
+min=l=>Math.min(...l)
+
+max=l=>Math.max(...l)
+
+/*Description:
+
+Your task is to make two functions, max and min that take a vector of integers list as input and outputs, respectively, the largest and lowest number in that vector.*/
